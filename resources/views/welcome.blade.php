6348f1de7969db1d6d919b80de6d15abb52fbec1
Evgeny <berezentsev.e@mail.ru>
2020-03-03 17:25:10 +0400

    создан контроллер, организован вывод тестовых данных

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h2>Lets Go!</h2>
+
+    @foreach($tasks as $task)
+        <div class="container d-flex justify-content-between border p-3">
+            <div>
+                @if($task->priority == 1)
+                    <h4>{{ $task->title }}</h4>
+                @elseif($task->priority == 2)
+                    <h4 class="text-primary">{{ $task->title }}</h4>
+                @elseif($task->priority == 3)
+                    <h4 class="text-danger">{{ $task->title }}</h4>
+                @endif
+            </div>
+            <div>
+                <a href="" class="btn btn-primary btn-sm">Редактировать</a>
+                <a href="" class="btn btn-success btn-sm">Выполнить</a>
+                <a href="" class="btn btn-danger btn-sm">Удалить</a>
+            </div>
+        </div>
+    @endforeach
+
 @endsection
 
